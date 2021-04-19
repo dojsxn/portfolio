154f4dbc838ea967461485941c35c088b259fac4
scdoja <create.jsxn@gmail.com>
2021-04-19 14:57:13 -0400

    Adjustments

--- portfolio/src/components/HomeProject3/HomeProject3.js
+++ portfolio/src/components/HomeProject3/HomeProject3.js
@@ -9,7 +9,7 @@ export const HomeProject3 = ()=>{
 		<div className="p-card">
 		<a className="p-title" href="/unfold" target="_blank">unfold</a>
 			<h2 className="p-title2">UX/UI Design, Front-End Development, Visual Responsive Web Design</h2>
-			<p>
+			<p className="p-content">
 			unfold was a project focused on creating a responsive landing page utilizing CSS Frameworks + Preprocessors.
 			unfold was a stepping stone project that allowed me to improve my front-end development skills and visual responsive web design skills.
 			</p>
