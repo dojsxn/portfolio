c55f8fa7c97a12bcff2bbe99ef97acf2d9b1e2ba
scdoja <create.jsxn@gmail.com>
2021-04-19 11:04:34 -0400

    AboutPage

--- portfolio/src/components/AboutEdu/AboutEdu.js
+++ portfolio/src/components/AboutEdu/AboutEdu.js
@@ -10,14 +10,14 @@ export const AboutEdu =()=>{
 	<h1 className="title">Education</h1>
 		<p className="e-title-2">Sheridan College - Interactive Media Management</p>
 		<p className="e-title-3">September 2020 to April 2021 - Mississauga, ON</p>
-		<p>Creating, managing and developing web and mobile content. Learning and focused on interactivity, front-end coding, social media, user experience (UX) and emerging mobile technologies.
+		<p className="e-content">Creating, managing and developing web and mobile content. Learning and focused on interactivity, front-end coding, social media, user experience (UX) and emerging mobile technologies.
 		As well, experiencing user-focused design applications and learning to create user interfaces with Adobe Creative Suites.</p>
 	</div>
 
 	<div className="edu">
 		<p className="e-title-2">Sheridan College - Advertising & Marketing Communications</p>
 		<p className="e-title-3">September 2017 to April 2020 - Mississauga, ON</p>
-		<p>Focused on the world of Advertising & Marketing Communications. In this program, I learned foundational advertising knowledge, media development, creative and account management.
+		<p className="e-content">Focused on the world of Advertising & Marketing Communications. In this program, I learned foundational advertising knowledge, media development, creative and account management.
 		Working with students and professors to develop many advertising campaigns and proposals to real clients.</p>
 	</div>
 </div>
--- portfolio/src/components/AboutExp/AboutExp.js
+++ portfolio/src/components/AboutExp/AboutExp.js
@@ -10,7 +10,7 @@ export const AboutExp = ()=>{
 		<h1 className="title">Experience</h1>
 			<p className="e-title-2">Social Media Coordinator - Sheridan College</p>
 				<p className="e-title-3">September 2019 to April 2020 - Mississauga, ON</p>
-				<p>I was responsible for communicating events and opportunities to students in the Advertising and Marketing Communications program.
+				<p className="e-content">I was responsible for communicating events and opportunities to students in the Advertising and Marketing Communications program.
 				I managed and planned events for several social media channels to promote in real time.
 				As well, managed the contact and communication of information regarding the Advertising and Marketing Communications program for social network users.
 				</p>
@@ -19,7 +19,7 @@ export const AboutExp = ()=>{
 	<div className="exps">
 			<p className="e-title-2">PAL Student Tutor - Sheridan College</p>
 				<p className="e-title-3">September 2018 to April 2019 - Mississauga, ON</p>
-				<p>Responsible for tutoring, teaching and aiding students at Sheridan College, during my winter semester term in 2019.
+				<p className="e-content">Responsible for tutoring, teaching and aiding students at Sheridan College, during my winter semester term in 2019.
 				I was tasked with helping students with English work such as revising, grammar checking and proof-reading student work.
 				</p>
 	</div>
