2a163e3d3e839eae5eeeefc01a2a86c0b46ff370
scdoja <create.jsxn@gmail.com>
2021-04-20 00:43:07 -0400

    AboutStyling

--- portfolio/src/components/AboutEdu/AboutEdu.js
+++ portfolio/src/components/AboutEdu/AboutEdu.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import './AboutEdu.scss';
 
-
-export const AboutEdu =()=>{
+export const AboutEdu = () =>{
 	return(
-<div className="aboutEducation">
 
-	<div className="edu1">
-	<h1 className="title">Education</h1>
-		<p className="e-title-2">Sheridan College - Interactive Media Management</p>
-		<p className="e-title-3">September 2020 to April 2021 - Mississauga, ON</p>
-		<p className="e-content">Creating, managing and developing web and mobile content. Learning and focused on interactivity, front-end coding, social media, user experience (UX) and emerging mobile technologies.
-		As well, experiencing user-focused design applications and learning to create user interfaces with Adobe Creative Suites.</p>
-	</div>
+	<div className="aboutEducation">
+		<div className="edu1">
+			<h1 className="title">Education</h1>
+				<p className="e-title-2">Sheridan College - Interactive Media Management</p>
+				<p className="e-title-3">September 2020 to April 2021 - Mississauga, ON</p>
+				<p className="e-content">Creating, managing and developing web and mobile content. Learning and focused on interactivity, front-end coding, social media, user experience (UX) and emerging mobile technologies.
+				As well, experiencing user-focused design applications and learning to create user interfaces with Adobe Creative Suites.</p>
+		</div>
 
-	<div className="edu">
-		<p className="e-title-2">Sheridan College - Advertising & Marketing Communications</p>
-		<p className="e-title-3">September 2017 to April 2020 - Mississauga, ON</p>
-		<p className="e-content">Focused on the world of Advertising & Marketing Communications. In this program, I learned foundational advertising knowledge, media development, creative and account management.
-		Working with students and professors to develop many advertising campaigns and proposals to real clients.</p>
+		<div className="edu">
+			<p className="e-title-2">Sheridan College - Advertising & Marketing Communications</p>
+			<p className="e-title-3">September 2017 to April 2020 - Mississauga, ON</p>
+			<p className="e-content">Focused on the world of Advertising & Marketing Communications. In this program, I learned foundational advertising knowledge, media development, creative and account management.
+			Working with students and professors to develop many advertising campaigns and proposals to real clients.</p>
+		</div>
 	</div>
-</div>
 
 	)
-
 }
 
 export default AboutEdu;
