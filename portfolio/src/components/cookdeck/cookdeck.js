3b1800ff7f8b49d4995e51ec3e920c9cb2913177
scdoja <create.jsxn@gmail.com>
2021-04-20 00:31:55 -0400

    Final Styling

--- portfolio/src/components/cookdeck/cookdeck.js
+++ portfolio/src/components/cookdeck/cookdeck.js
@@ -23,33 +23,35 @@ export const cookDeck = ()=>{
 		<div className="pro-overview">
 
 		<div className="pro-txt">
-		<p>Overview</p>
-			<p>cookdeck is a recipe web application that aims to provide recipes based on lifestyles. This app helps users view and select recipes based on their lives & lifestyle, providing a wide range of recipes for multiple lifestyles. </p>
+		<p className="pro-txt-title">Overview</p>
+			<p className="pro-txt-content">cookdeck is a recipe web application that aims to provide recipes based on lifestyles. This app helps users view and select recipes based on their lives & lifestyle, providing a wide range of recipes for multiple lifestyles. </p>
 
-		<p>Tools</p>
-			<p>HTML, CSS, JS, MySQL + PHP</p>
+		<p className="pro-txt-title2">Tools</p>
+			<p className="pro-txt-content">HTML, CSS, JS, MySQL + PHP</p>
 
-		<p>My Roles & Tasks</p>
-			<p>Front-End & Back-End Development, CMS Organization</p>
+		<p className="pro-txt-title2">My Roles & Tasks</p>
+			<p className="pro-txt-content">Front-End & Back-End Development, CMS Organization</p>
 
-		<p>Process</p>
-			<p>User Research + Testing, Prototyping, Front-End & Back-End Development.</p>
+		<p className="pro-txt-title2">Process</p>
+			<p className="pro-txt-content">User Research + Testing, Prototyping, Front-End & Back-End Development.</p>
 		</div>
 
-		<div className="pro-img">
-			<p>test</p>
-		</div>
+			<div className="pro-img">
+				<p>test</p>
+			</div>
 		</div>
 
 
 		<div className="pro-challenge">
-		<p>Challenge Statement</p>
-			<p>“How can cookdeck tackle the challenge of constant changing lifestyles and dietary needs among Canadians?” </p>
+		<div className="pro-txt">
+		<p className="pro-txt-title">Challenge Statement</p>
+			<p className="ptxt4">“How can cookdeck tackle the challenge of constant changing lifestyles and dietary needs among Canadians?” </p>
+		</div>
 		</div>
 
 		<div className="pro-solution">
-		<p>Solution</p>
-			<p>The idea was to promote home cooking & healthy eating with an application that allows users to retrieve recipes based on lifestyles. The solution aims to provides users with relevant recipes based on their selected lifestyles. Recipes can vary in terms of ingredients required, prepping & cooking time and nutritional information. </p>
+		<p className="pro-txt-title">Solution</p>
+			<p className="ptxt">The idea was to promote home cooking & healthy eating with an application that allows users to retrieve recipes based on lifestyles. The solution aims to provides users with relevant recipes based on their selected lifestyles. Recipes can vary in terms of ingredients required, prepping & cooking time and nutritional information. </p>
 		</div>
 
 		<div className="pro-userflow">
@@ -66,10 +68,10 @@ export const cookDeck = ()=>{
 
 		<div className="pro-future">
 		<p className="pro-txt-title">Future Outlook & Continuing Steps</p>
-			<p>In the future, I would like to develop cookdeck into a mobile application that integrates the functionality of the web application. With my current concept & prototype, it would be interesting to see how I can further develop cookdeck into a full application.</p>
+			<p className="ptxt2">In the future, I would like to develop cookdeck into a mobile application that integrates the functionality of the web application. With my current concept & prototype, it would be interesting to see how I can further develop cookdeck into a full application.</p>
 
 		<p className="pro-txt-title">Learning & Lessons</p>
-			<p>With cookdeck, this was my first time designing and developing an application that focuses on health & food. I wanted cookdeck to be different in terms of functionality and design, where users can easily access relevant recipes in a unique web experience that is different from traditional recipe websites.</p>
+			<p className="ptxt">With cookdeck, this was my first time designing and developing an application that focuses on health & food. I wanted cookdeck to be different in terms of functionality and design, where users can easily access relevant recipes in a unique web experience that is different from traditional recipe websites.</p>
 		</div>
 
 
