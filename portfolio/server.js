cc3d08df45f27db003bf723dcf43275e4d7f25fe
scdoja <create.jsxn@gmail.com>
2021-04-22 21:17:45 -0400

    added previews

--- portfolio/server.js
+++ portfolio/server.js
@@ -22,8 +22,6 @@ mongoose.connection.on('connected', ()=>{
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
 
-
-//HTTP request logger
 app.use(morgan('tiny'));
 app.use('/api', routes);
 app.use('/capi', require('./routes/capi'))
